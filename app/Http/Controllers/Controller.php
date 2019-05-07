9602edc750aef533fb92450b06948c4b8849e026
Tim Joosten <Tjoosten3@gmail.com>
2019-05-08 00:49:36 +0200

    Implementatie helper function auth gebruiker
    [SIGNED OFF]: Tim Joosten <topairy@gmail.com>

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Class Controller 
+ * 
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Helper function for getting the information about the authenticated user. 
+     * 
+     * @return User
+     */
+    public function getAuthenticatedUser(): User
+    {
+        return auth()->user();
+    }
 }
