a6e1891cf7bb89844818d663612805eb989a84df
Tim Joosten <Tjoosten3@gmail.com>
2019-05-15 12:23:58 +0200

    Update

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,6 +6,11 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\ContactValidator;
 
+/**
+ * Class ContactController
+ * 
+ * @package App\Http\Controller
+ */
 class ContactController extends Controller
 {
     public function index(): Renderable 
